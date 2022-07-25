bfb26b55cae82e7ebeb28c82febff93b39b8f386
ChaosCantrip <james.donald.02@gmail.com>
2022-07-25 05:10:50 +0100

    Added Effect.duration

--- definitions/Effect.py
+++ definitions/Effect.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 class Effect:
 
-    def __init__(self, effectid: int, name: str):
+    def __init__(self, effectid: int, name: str, duration: timedelta):
         self.id = effectid
         self.name = name
+        self.duration = duration
