c520140df55ab5e763aec6648e26bb5dd79131d6
ChaosCantrip <james.donald.02@gmail.com>
2022-07-25 07:53:41 +0100

    Added $use command

--- cogs/consumables.py
+++ cogs/consumables.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import tasks, commands
+from definitions import Member, Item
 
 import secret
 
@@ -13,6 +14,24 @@ class Consumables(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def use(self, ctx, *, search):
+        member = Member.get(ctx.author.id)
+        item = Item.search(search)
+
+        if type(item) is not Item.ConsumableItem:
+            await ctx.send(f"**{item.name}** is not a consumable item!")
+            return
+
+        if item.id not in member.get_inventory():
+            await ctx.send(f"You don't have a **{item.name}**!")
+            return
+
+        member.apply_effect(item.effect, item.effectduration)
+        member.remove_from_inventory(item)
+        await ctx.send(f"You used a **{item.name}**!")
+
 
 
 def setup(bot):
