4b303a64e483d3edfa5f53599c522cf346fa734c
ChaosCantrip <dev@chaoscantrip.com>
2024-06-29 23:09:14 +0100

    Changed on_ready() to give better printout and embed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 import os
-
+import colorama
 import discord
 from discord.ext import commands
+
+colorama.init(autoreset=True)
 
 import SharkBot
 
@@ -11,10 +13,20 @@ bot = commands.Bot(command_prefix="$", intents=discord.Intents.all(), case_insen
 
 @bot.event
 async def on_ready():
-    print("Bot is ready")
+    print(colorama.Fore.GREEN + "SharkBot connected to Discord")
+    print(colorama.Fore.YELLOW + f"\t- Account: {bot.user}")
+    print(colorama.Fore.YELLOW + f"\t- User ID: {bot.user.id}")
+
+    embed = discord.Embed()
+    embed.title = "SharkBot is up and running!"
+    embed.description = "SharkBot is now connected to Discord."
+    embed.set_thumbnail(url=bot.user.display_avatar.url)
+    embed.colour = discord.Colour.green()
+
     app_info = await bot.application_info()
     owner = app_info.owner
-    await owner.send("SharkBot up and running!")
+    await owner.send(embed=embed)
+    await bot.change_presence(status=discord.Status.online)
 
 
 # ===== Main =====
