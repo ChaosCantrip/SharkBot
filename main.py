006bd272f0b3500e76cbcf52faeae5c295dbb288
ChaosCantrip <dev@chaoscantrip.com>
2024-06-29 23:07:15 +0100

    Added basic bot framework

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all(), case_insensitive=True)
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+    app_info = await bot.application_info()
+    owner = app_info.owner
+    await owner.send("SharkBot up and running!")
+
+
+# ===== Main =====
+
+
+async def main():
+
+    async with bot:
+        await bot.start(os.environ.get("DISCORD_TOKEN"))
+
+asyncio.run(main())
