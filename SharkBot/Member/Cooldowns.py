87c2de4eccd031e290f440dbb02592282497bd02
ChaosCantrip <dev@chaoscantrip.com>
2024-06-30 01:53:08 +0100

    Added Cooldown class and constructor

--- SharkBot/Member/Cooldowns.py
+++ SharkBot/Member/Cooldowns.py
@@ -1,2 +1,14 @@
+from datetime import timedelta, datetime
+
+_TIME_FORMAT = "%d/%m/%Y-%H:%M:%S"
+
+
+class Cooldown:
+
+    def __init__(self, duration: timedelta, expiry: str):
+        self.duration: timedelta = duration
+        self.expiry: datetime = datetime.strptime(expiry, _TIME_FORMAT)
+
+
 class Cooldowns:
     pass
