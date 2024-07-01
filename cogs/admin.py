7f7a8dc1f2805b7b5e3d2ed2eafe4b5c140e30b8
ChaosCantrip <dev@chaoscantrip.com>
2024-07-01 18:47:04 +0100

    Added $op and $deop commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,6 +8,21 @@ class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command()
+    @SharkBot.Checks.Permissions.is_admin()
+    async def op(self, ctx: commands.Context, member: discord.Member):
+        sharkbot_member = SharkBot.Member.get(member.id)
+        sharkbot_member.permissions.admin = True
+        sharkbot_member.write_data()
+        await ctx.send(f"{member.mention} is now an admin.")
+
+    @commands.command()
+    @SharkBot.Checks.Permissions.is_admin()
+    async def deop(self, ctx: commands.Context, member: discord.Member):
+        sharkbot_member = SharkBot.Member.get(member.id)
+        sharkbot_member.permissions.admin = False
+        sharkbot_member.write_data()
+        await ctx.send(f"{member.mention} is no longer an admin.")
 
 async def setup(bot):
     await bot.add_cog(Admin(bot))
