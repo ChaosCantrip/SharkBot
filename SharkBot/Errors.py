d4e65bc660a7fb01f637908eaeb288c17b762690
james.donald.02 <james.donald.02@gmail.com>
2024-07-01 09:38:29 +0000

    Added docstrings to SharkError class

--- SharkBot/Errors.py
+++ SharkBot/Errors.py
@@ -3,9 +3,14 @@ from discord.ext import commands
 
 
 class SharkError(Exception):
+    """Base class for all custom SharkBot Errors"""
 
     async def report(self, ctx: commands.Context):
+        """Method for reporting error to developer"""
+
         return
 
     async def handler(self, ctx: commands.Context) -> bool:
+        """Method for responding to the invoking command with a coherent response"""
+
         return False
