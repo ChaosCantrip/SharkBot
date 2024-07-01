b833167736639fce85f306668bbe9079dbacefee
ChaosCantrip <dev@chaoscantrip.com>
2024-07-01 18:45:52 +0100

    Changed is_admin() to automatically allow the owner

--- SharkBot/Checks/Permissions.py
+++ SharkBot/Checks/Permissions.py
@@ -6,6 +6,10 @@ import SharkBot
 def is_admin():
     async def predicate(ctx: commands.Context):
         member = SharkBot.Member.get(ctx.author.id)
-        if not member.permissions.admin:
-            raise commands.MissingPermissions(["admin"])
+        if member.permissions.admin:
+            return True
+        app_info = await ctx.bot.application_info()
+        if ctx.author.id == app_info.owner.id:
+            return True
+        raise commands.MissingPermissions(["admin"])
     return commands.check(predicate)
