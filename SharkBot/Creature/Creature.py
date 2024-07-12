c746eeb0df88c62189d2900a9eff0ab72d556e3d
ChaosCantrip <dev@chaoscantrip.com>
2024-07-12 21:13:25 +0100

    Added Creature class constructor and properties

--- SharkBot/Creature/Creature.py
+++ SharkBot/Creature/Creature.py
@@ -1,2 +1,30 @@
+from typing import Self
+
+from SharkBot.Creature import BaseStats
+
+
 class Creature:
-    pass
+    creatures: list[Self] = []
+    creatures_map: dict[str, Self] = {}
+
+    def __init__(self, id: str, name: str, base_stats: list[int | float]):
+        self._id: str = id
+        self._name: str = name
+        self._base_stats: BaseStats = BaseStats(*base_stats)
+
+        self.creatures.append(self)
+        self.creatures_map[id] = self
+
+    # ===== Properties =====
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def base_stats(self) -> BaseStats:
+        return self._base_stats
