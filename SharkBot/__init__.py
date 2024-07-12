0bf200bc6b03633f42c01dd80aabf33504ca430a
ChaosCantrip <dev@chaoscantrip.com>
2024-07-13 00:33:20 +0100

    Added Banner class

--- SharkBot/__init__.py
+++ SharkBot/__init__.py
@@ -3,5 +3,6 @@ from . import Errors
 from . import Checks
 from . import Rarity
 from . import Alignment
+from .Banner import Banner
 from .Creature import Creature, MemberCreature
 from .Member import Member
