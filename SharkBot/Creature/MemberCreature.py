85fcb10b3e648171a3acc4f14dca9706cc1b6b00
ChaosCantrip <dev@chaoscantrip.com>
2024-07-12 21:25:43 +0100

    Added MemberCreature properties and methods

--- SharkBot/Creature/MemberCreature.py
+++ SharkBot/Creature/MemberCreature.py
@@ -1,2 +1,63 @@
+from . import Creature
+
+LEVEL_THRESHOLDS: list[int] = [
+    100,
+    200,
+    300,
+    400,
+    500,
+    700,
+    900,
+    1200,
+    1500,
+    2000,
+    2500,
+    3000,
+    4000,
+    5000,
+    6000,
+    7000,
+    8000,
+    9000,
+    10000,
+    15000
+]
+
+
 class MemberCreature:
-    pass
+
+    def __init__(self, base_creature_id: str, power: int, last_handled_level: int):
+        self._base_creature: Creature = Creature.get(base_creature_id)
+        self._power: int = power
+        self.last_handled_level: int = last_handled_level
+
+    # ===== Properties =====
+
+    @property
+    def base_creature(self) -> Creature:
+        return self._base_creature
+
+    @property
+    def power(self) -> int:
+        return self._power
+
+    @property
+    def level(self) -> int:
+        for i, threshold in enumerate(LEVEL_THRESHOLDS):
+            if self.power < threshold:
+                return i
+        return len(LEVEL_THRESHOLDS)
+
+    # ===== Abilities =====
+
+    @property
+    def leader_skill_description(self):
+        return self._base_creature.leader_skill_description(self.level)
+
+    @property
+    def support_skill_description(self):
+        return self._base_creature.support_skill_description(self.level)
+
+    @property
+    def unique_ability_description(self):
+        return self._base_creature.unique_ability_description(self.level)
