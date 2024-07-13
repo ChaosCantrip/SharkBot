7a4dfa7e4529fab9aed95659c1170a6f9d59bf79
ChaosCantrip <dev@chaoscantrip.com>
2024-07-13 01:51:02 +0100

    Added Rarity emojis

--- SharkBot/Rarity.py
+++ SharkBot/Rarity.py
@@ -1,7 +1,8 @@
 class Rarity:
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, emoji: str):
         self._name: str = name
+        self._emoji: str = emoji
 
     # ===== Properties =====
 
@@ -9,8 +10,12 @@ class Rarity:
     def name(self) -> str:
         return self._name
 
+    @property
+    def emoji(self) -> str:
+        return self._emoji
 
-COMMON = Rarity("Common")
-RARE = Rarity("Rare")
-LEGENDARY = Rarity("Legendary")
-MYTHIC = Rarity("Mythic")
+
+COMMON = Rarity("Common", "<:common:1261483012864872529>")
+RARE = Rarity("Rare", "<:rare:1261483011766222998>")
+LEGENDARY = Rarity("Legendary", "<:legendary:1261483010214334567>")
+MYTHIC = Rarity("Mythic", "<:mythic:1261483009312292905>")
