2666b036bafebdd9994843a3be5c95e179b0ce80
ChaosCantrip <dev@chaoscantrip.com>
2024-07-13 01:02:17 +0100

    Added temporary pull command

--- cogs/banners.py
+++ cogs/banners.py
@@ -4,12 +4,14 @@ import SharkBot
 import string
 
 active_banners: dict[SharkBot.Banner, dict[str, SharkBot.BuyOption]] = {}
+active_buy_options: dict[str, tuple[SharkBot.Banner, SharkBot.BuyOption]] = {}
 
 for _banner_index, _banner in enumerate(SharkBot.Banner.get_active_banners()):
     active_banners[_banner] = {}
     for _buy_option_index, _buy_option in enumerate(_banner.buy_options):
         letter = string.ascii_lowercase[_buy_option_index]
         active_banners[_banner][f"{_banner_index + 1}{letter}"] = _buy_option
+        active_buy_options[f"{_banner_index + 1}{letter}"] = (_banner, _buy_option)
 
 
 class Banners(commands.Cog):
@@ -29,6 +31,15 @@ class Banners(commands.Cog):
                 inline=False
             )
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def pull(self, ctx: commands.Context, banner_id: str):
+        banner_id = banner_id.lower()
+        if banner_id not in active_buy_options:
+            await ctx.send("Invalid banner ID.")
+            return
+        banner, buy_option = active_buy_options[banner_id]
+        await ctx.send(f"Pulled from {banner.name} for {buy_option.cost}.")
 
 
 async def setup(bot):
