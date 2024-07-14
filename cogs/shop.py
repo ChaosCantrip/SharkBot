35f5384061affb53f18527cfcaa80690315037e3
ChaosCantrip <dev@chaoscantrip.com>
2024-07-14 15:29:55 +0100

    Added $buy command

--- cogs/shop.py
+++ cogs/shop.py
@@ -41,6 +41,64 @@ class Shop(commands.Cog):
         embed.set_footer(text=f"Shop resets in {humanize.naturaldelta(reset_time_remaining)}")
         await ctx.send(embed=embed)
 
+    @commands.command()
+    async def buy(self, ctx: commands.Context, item_id: str, number: int = 1):
+        item_id = item_id.lower()
+        member = SharkBot.Member.get(ctx.author.id)
+        if member.shop_data.needs_reset:
+            member.shop_data.reset()
+        if item_id not in shop_items:
+            embed = discord.Embed()
+            embed.colour = discord.Colour.red()
+            embed.description = "Invalid item ID."
+            await ctx.send(embed=embed)
+            return
+        item = shop_items[item_id]
+        if item.limit is not None and member.shop_data.get_times_bought(item.id) >= item.limit:
+            embed = discord.Embed()
+            embed.title = "Limit Reached"
+            embed.colour = discord.Colour.red()
+            embed.description = "You have reached the limit for this item."
+            await ctx.send(embed=embed)
+            return
+        if item.limit is not None and member.shop_data.get_times_bought(item.id) + number > item.limit:
+            embed = discord.Embed()
+            embed.title = "Not Enough Remaining"
+            embed.colour = discord.Colour.red()
+            embed.set_thumbnail(url=ctx.author.display_avatar.url)
+            embed.description = f"You can only buy {item.limit - member.shop_data.get_times_bought(item.id)} more of this item."
+            await ctx.send(embed=embed)
+            return
+        if type(item.cost) is SharkBot.Costs.GemCost:
+            if member.gems < item.cost.amount * number:
+                embed = discord.Embed()
+                embed.title = f"Not Enough Gems"
+                embed.set_thumbnail(url=ctx.author.display_avatar.url)
+                embed.colour = discord.Colour.red()
+                embed.description = f"You only have :gem: **{member.gems}**"
+                await ctx.send(embed=embed)
+                return
+            member.gems -= item.cost.amount * number
+        elif type(item.cost) is SharkBot.Costs.MoneyCost:
+            if member.balance < item.cost.amount * number:
+                embed = discord.Embed()
+                embed.title = f"Not Enough Money"
+                embed.set_thumbnail(url=ctx.author.display_avatar.url)
+                embed.description = f"You only have :dollar: **{member.balance}**"
+                embed.colour = discord.Colour.red()
+                await ctx.send(embed=embed)
+                return
+            member.balance -= item.cost.amount * number
+        member.shop_data.add_times_bought(item.id, number)
+        if type(item.item) is SharkBot.Shop.GemsItem:
+            member.gems += item.item.amount * number
+        embed = discord.Embed()
+        embed.title = f"{ctx.author.display_name}'s Purchase"
+        embed.set_thumbnail(url=ctx.author.display_avatar.url)
+        embed.description = f"Bought {number}x **{item.item}** for **{item.cost}**."
+        embed.colour = discord.Colour.green()
+        await ctx.send(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Shop(bot))
